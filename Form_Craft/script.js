45020c8fd50733aeb60cea97c2bde3e17fbff38f
Arindam2003 <dindaarindam000@gmail.com>
2024-12-16 00:59:57 +0530

    username complete

--- Form_Craft/script.js
+++ Form_Craft/script.js
@@ -1,6 +1,11 @@
 function validate()
 {
-
+    var h=document.getElementsByTagName("h2")[0];
+    h.style.color="red";
+    var fname=document.getElementById("fName");
+    var sname=document.getElementById("sName");
+    var flag=true;
+    if(fname.value.length==0)
+        flag=false;
+    return flag;
 }
-
-function 
